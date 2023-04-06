0070a17692522f7972e266e7f012bf4ac02a5fb3
GameW1zard <rivenloya@gmail.com>
2023-04-06 18:40:39 -0500

    function update

--- index.js
+++ index.js
@@ -3,35 +3,66 @@ const inquirer = require('inquirer');
 const fs = require('fs')
 const generateMarkdown = require('./utils/generateMarkdown.js')
 var ProjectTitle = process.argv[2]
+var data = {
+    title: "",
+    description: "",
+    installation: "",
+    usage: "",
+    contribution: "",
+    testingInstructions: "",
+}
 // TODO: Create an array of questions for user input
-const questions = ["what is the name of your project?", "please discribe your project", "how do you install your project?"];
-
+const questions = ["what is the name of your project?",
+"please discribe your project (eg: its a calculator)",
+"how do you install your project? (eg: open in webrowser)",
+"How is your project used?",
+"Who worked on this project?",
+"Please enter short test instructions for your project",
+"What licence are you using for your project",
+"please enter your Github username",
+"please enter a email for awnsering questions about your project"
+]
 // TODO: Create a function to write README file
-function writeToFile(ProjectTitle) {
+function writeToFile(ProjectTitle) 
+{
     inquirer
         .prompt([
-            {type: 'input', message: questions[0], name: 'title'}
-
-
-
-
+            {type: 'input', message: questions[0], name: 'title'},
+            {type: 'input', message: questions[1], name: 'description'},
+            {type: 'input', message: questions[2], name: 'installation'},
+            {type: 'input', message: questions[3], name: 'usage'},
+            {type: 'input', message: questions[4], name: 'contribution'},
+            {type: 'input', message: questions[5], name: 'testingInstructions'},
+            {type: 'input', message: questions[7], name: 'guthubusername'},
+            {type: 'input', message: questions[8], name: 'email'},
+            {
+                type: 'list',
+                message: questions[6],
+                name: 'license',
+                choices: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]
+            }
         ])
 
-.then (function (response){
-console.log(response.title)
+        .then (function (response){
 
-fs.writeFile("./Output/" + ProjectTitle + '.md', response.title, (err) => err ? console.error(err) : console.log("Write success!"))
+                data.title = response.title
+                data.description = response.description
+                data.installation = response.installation
+                data.usage = response.usage
+                data.contribution = response.contribution
+                data.testingInstructions = response.testingInstructions
+                data.license = response.license
+                data.githubusername = response.githubusername
+                data.email = response.email
+                console.log(response.license)
 
-
-
-
-})
-
-
+                generateMarkdown.makeMD(data)
+        })
 }
 
 // TODO: Create a function to initialize app
 function init() {writeToFile(ProjectTitle)}
 
 // Function call to initialize app
+//generateMarkdown.testfunc("Hello world")
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+const fs = require('fs')
 function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
@@ -11,10 +12,24 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function makeMD(data) {
+  fs.writeFile(
+    `./Output/ ${data.title} .md`,
+     `# ${data.title}
+      \n \n ## Description \n ${data.description}
+      \n \n ## Installation \n ${data.installation}
+      \n \n ## Usage \n ${data.usage}
+      \n \n ## Contribution \n ${data.contribution}
+      \n \n ## Testing instructions \n ${data.testingInstructions}
+      \n \n ## Licence \n ${data.license}
+      \n \n ## Contact information \n ${data.username} \n ${data.email}`,
+       
+    (err) => err ? console.error(err) : console.log("Write success!"))
 }
-
-module.exports = generateMarkdown;
+const testfunc = function (string) {console.log(string)}
+//module.exports = generateMarkdown;
+exports.testfunc = testfunc;
+exports.renderLicenseBadge = renderLicenseBadge;
+exports.renderLicenseLink = renderLicenseLink;
+exports.renderLicenseSection = renderLicenseSection;
+exports.makeMD = makeMD;
